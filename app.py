99e8d301205aa46e8c51c653b1c45c3cf56cde4f
JB.py <108344718+jbseooo@users.noreply.github.com>
2024-02-20 22:40:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
     return encoded
-def img_to_html(img_path):
-    img_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
-      img_to_bytes(img_path)
+
+def img_to_html(img_path, max_width='100%'):
+    img_html = "<img src='data:image/png;base64,{}' class='img-fluid' style='max-width:{};'>".format(
+        img_to_bytes(img_path),
+        max_width
     )
     return img_html
+
+st.markdown(img_to_html('image/12.png', max_width='100%'), unsafe_allow_html=True)
+
 
 st.markdown(img_to_html('image/12.png'), unsafe_allow_html=True)
 
